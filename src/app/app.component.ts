ac3a3da13ae0d055b3a22812aa3868ebc7764e76
epac-thechnologies <chihebs@epactunis.com>
2020-06-04 16:48:17 +0100

    changed version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-pipeline-v1';
+  title = 'angular-pipeline-v2';
 }
