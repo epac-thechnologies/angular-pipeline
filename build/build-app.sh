aea26e73235bea081dcca9a7cc5c8e1328d7b0d0
epac-thechnologies <chihebs@epactunis.com>
2020-06-04 17:09:46 +0100

    update jenkinsfile

--- build/build-app.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export PATH=/sbin:/usr/sbin:/usr/bin:/usr/local/bin
-pwd
-npm cache clean --force
-npm config set unsafe-perm true
-npm install
-ng build --prod
-docker build -t angular-pipeline .
